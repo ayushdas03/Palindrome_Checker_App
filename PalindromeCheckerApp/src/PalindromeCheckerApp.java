c525142f2aab70a5fef76612e70f7a0c68f37adf
adarsh0error <adarshmittal621@gmail.com>
2026-02-24 15:44:23 +0530

    UC2: Print a Hardcoded Palindrome Result

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,5 +1,12 @@
 public class PalindromeCheckerApp {
     static void main() {
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("======================================");
+        System.out.println(" Palindrome Checker - UC2 ");
+        System.out.println("======================================");
+        String input = "madam";
+        boolean isPalindrome = true;
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
     }
 }
+
