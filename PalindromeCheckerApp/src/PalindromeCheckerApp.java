c546bd17ae2594b3642c1532e62cc5893efe82df
adarsh0error <adarshmittal621@gmail.com>
2026-02-24 15:55:48 +0530

    UC4: Character Array Based Palindrome Check

--- PalindromeCheckerApp/src/PalindromeCheckerApp.java
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -1,12 +1,43 @@
+import java.util.Scanner;
+
 public class PalindromeCheckerApp {
     static void main() {
+        System.out.println(" Character Array Based Palindrome Check ");
         System.out.println("======================================");
-        System.out.println(" Palindrome Checker - UC2 ");
-        System.out.println("======================================");
-        String input = "madam";
-        boolean isPalindrome = true;
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + isPalindrome);
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = sc.nextLine();
+
+        boolean result = isPalindrome(input);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        sc.close();
+    }
+
+    /**
+     * Checks palindrome using character array comparison.
+     *
+     * @param s Input string
+     * @return true if palindrome, otherwise false
+     */
+    private static boolean isPalindrome(String s) {
+
+        char[] chars = s.toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
     }
 }
 
